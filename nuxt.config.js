5f8ab8a820d9b23ee8eb0d8c70d7a6441b0ac0d9
Dean Tawonezvi <deantawonezvi1@gmail.com>
2021-11-22 17:38:51 +0200

    Initial project setup

---
+++ nuxt.config.js
@@ -0,0 +1,55 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Star Wars',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  css: [
+  ],
+
+  plugins: [
+  ],
+
+  components: true,
+
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@chakra-ui/nuxt',
+    '@nuxtjs/emotion',
+    '@nuxtjs/axios',
+    '@nuxtjs/apollo',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+
+  // Apollo configuration
+  apollo: {
+    clientConfigs: {
+      default: {
+        httpEndpoint: 'https://star-wars-api.deant.work/graphql',
+      },
+    },
+  },
+}
